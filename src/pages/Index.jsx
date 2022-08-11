d63f2dd737bc228f9bbb60aa9aeb5a366180dab9
FranciscoWork <jose_fko_92@hotmail.com>
2022-08-11 15:28:54 -0500

    bug pages

--- src/pages/Index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as Home } from './Home';
-export { default as Login } from './Login';
-export {default as ProductDetail } from './ProductDetail';
-export {default as Purchases } from './Purchases';
