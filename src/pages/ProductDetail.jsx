ef06480d05644a549d37ec7b180beea6e134dada
francisco-desktop <jose_fko_92@hotmail.com>
2022-08-11 22:55:46 -0500

    refresh detailProduct

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -2,37 +2,46 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addProductThunk } from "../store/slices/cart.slice";
+import { getProductsThunk } from "../store/slices/products.slice";
 
 const ProductDetail = () => {
+  const { id } = useParams();
   const products = useSelector((state) => state.products);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [counter, setCounter] = useState(1);
   const token = localStorage.getItem("token");
+  const [product, setProduct] = useState({});
+
+  
+
+  useEffect(() => {
+    dispatch(getProductsThunk())
+    const productfind = products.find((item) => item.id === Number(id));
+    setProduct(productfind)
+  }, [id , products]);
 
 
-  const { id } = useParams();
-  const product = products.find((item) => item.id === Number(id));
 
   const addProduct = (id) => {
-    if(token){
-        const data = { id: id, quantity: counter };
-        dispatch(addProductThunk(data));
-    }else{
-        navigate("/login")
+    if (token) {
+      const data = { id: id, quantity: counter };
+      dispatch(addProductThunk(data));
+    } else {
+      navigate("/login");
     }
   };
 
   return (
     <div className="detailProduct">
       <div className="bxImg">
-        <img src={product.productImgs[0]} alt={product?.title} />
+        <img src={product?.productImgs?.[0]} alt={product?.title} />
       </div>
       <h2>{product?.title}</h2>
       <div className="price_quantity">
         <div className="price">
           <p>Price</p>
-          <p>$ {product.price}</p>
+          <p>$ {product?.price}</p>
         </div>
         <div className="quantity">
           <p>Quantity</p>
@@ -57,7 +66,7 @@ const ProductDetail = () => {
       <button onClick={() => addProduct(product.id)} className="addCart">
         Add to cart{" "}
       </button>
-      <p className="description">{product.description}</p>
+      <p className="description">{product?.description}</p>
     </div>
   );
 };
